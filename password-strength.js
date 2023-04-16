1c516d6ae83f84c813a287bb7adc581d52699675
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-04-16 20:55:02 +0200

    UI Update and icon

--- password-strength.js
+++ password-strength.js
@@ -3,7 +3,25 @@ function show_password() {
     var password = document.getElementById("user-password");
     if (password.type === "password") {
         password.type = "text";
-    } else {
+    }
+    else {
         password.type = "password";
     }
+    if (password.value.length <= 10) {
+        console.log("pas fou") 
+    }
+}
+
+/* Level of security of the password */
+function security() {
+    let i = 0;
+    let text = document.getElementsByClassName("security-level")
+    if (password.length <= 10) {
+        text.textContent = "Mauvais";
+    }
+}
+
+var password = document.getElementById("user-password");
+if (password.value.length > 2) {
+    console.log("pas fou") 
 }
